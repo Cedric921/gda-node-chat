058107771e71f8baf309bd83f64b15512b784680
Nkera Urbain <urbainmuhamiriza1990@gmail.com>
2022-10-17 13:26:51 +0200

    création du formulaire d'envoi message

---
+++ frontend/src/Envoiemessage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Envoiemessage extends Component {
+    render() {
+    return (
+      <div>
+<input type="text" className='Mesenv text-primary form-control' placeholder='Votre Message ici'></input>
+<button className='btnenv text-primary'> Envoie Message</button>
+      </div>
+    )
+  }
+}
