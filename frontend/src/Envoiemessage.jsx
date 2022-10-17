7017852f5774f490640262ff053a5cb972e22c64
Nkera Urbain <urbainmuhamiriza1990@gmail.com>
2022-10-17 16:37:43 +0200

    update the formular

--- frontend/src/Envoiemessage.jsx
+++ frontend/src/Envoiemessage.jsx
@@ -3,12 +3,21 @@ import React, { Component } from 'react'
 export default class Envoiemessage extends Component {
     render() {
     return (
+  <div className='main d-flex'>
       <div className='div d-flex  justify-content-center flex-column form-control w-50'>
-        <p className='titre text-center text-primary text-uppercase'> Formulaire d'envoie Message</p>
+        <p className='titre text-center text-primary text-uppercase fw-bold'> Formulaire d'envoie Message</p>
         <button className='btn-primary btn m-1'> Message de...</button>
         <textarea className='textarea  text-center' placeholder='Votre Message ici'></textarea>
-        <button className='btn m-2 btn-primary'>Envoi Message</button>
+        <button className='btn m-2 btn-primary '>Envoi Message</button>
       </div>
+
+    <div className='div d-flex  justify-content-center flex-column form-control w-50'>
+      <button className='titre text-center text-primary'> Message reçu</button>
+      <button className='titre text-center text-primary'> Message reçu</button>
+      <button className='titre text-center text-primary'> Message reçu</button>
+    </div>
+  </div>
+  
     )
   }
 }
