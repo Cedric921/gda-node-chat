acf3b416eaafacc89a82a0ff3f2d94e064b6aeab
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-17 14:12:11 +0200

    feat : connect to the serve

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,25 @@
 // add all modules can be used
-let http = require('http'),
-    url = require('url'),
-    fs = require('fs')
+const http = require("http");
+const  url = require("url");
+const  fs = require("fs");
+
+// importation of messages Object
+
+const messages = require("./messages");
+
+const clients = [];
+const PORT = 5000;
+
+// Connection to the server
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  fs.readFile('../frontend/index.html', (err, data)=>{
+    res.end(data)
+  })
+});
+
+server.listen(PORT, () =>
+  console.log(`Notre app est lancée sur : http://localhost:${PORT}`)
+);
---
+++ backend/messages.js
@@ -0,0 +1,9 @@
+const messages = [
+    {
+        message : 'Lorem ipsum dolor sit amet consectetur adipisicing',
+        auteur : 'tata',
+    },
+   
+]
+
+module.exports = messages
