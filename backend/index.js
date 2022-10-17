8df498ca67092fff538c6b96e14da180f857a1fb
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-17 16:17:32 +0200

    feat : add route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const  fs = require("fs");
 // importation of messages Object
 
 const messages = require("./messages");
+const { unescape } = require("querystring");
 
 const clients = [];
 const PORT = 5000;
@@ -15,9 +16,31 @@ const PORT = 5000;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  fs.readFile('../frontend/index.html', (err, data)=>{
-    res.end(data)
-  })
+
+    const url_path = url.parse(req.url);
+    const msg = url_path;
+        // console.log(msg);
+    if(url_path.pathname == '/'){
+        fs.readFile('./index.html', (err, data)=>{
+            console.log("/")
+            res.end(JSON.stringify(messages))
+          })
+    }else if(url_path.pathname == '/add'){
+        const message = url_path.query.split('=')[1]
+        console.log('/add one message');
+        console.log('message => ', message)
+        messages.push(message)
+        fs.writeFile('messages.json', (err, data)=>{
+
+            console.log(data);
+        })
+
+
+    } else {
+        const message = url_path.query
+        console.log('/add',url_path,message);
+    }
+  
 });
 
 server.listen(PORT, () =>
--- backend/messages.js
+++
@@ -1,9 +0,0 @@
-const messages = [
-    {
-        message : 'Lorem ipsum dolor sit amet consectetur adipisicing',
-        auteur : 'tata',
-    },
-   
-]
-
-module.exports = messages
