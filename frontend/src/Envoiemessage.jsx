02b0ed3f99715eec0ab8b28bbb4158d5a87635cb
Nkera Urbain <urbainmuhamiriza1990@gmail.com>
2022-10-18 10:36:35 +0200

    update the formular

--- frontend/src/Envoiemessage.jsx
+++ frontend/src/Envoiemessage.jsx
@@ -7,8 +7,8 @@ export default class Envoiemessage extends Component {
       <div className='div d-flex  justify-content-center flex-column form-control w-50'>
         <p className='titre text-center text-primary text-uppercase fw-bold'> Formulaire d'envoie Message</p>
         <button className='btn-primary btn m-1'> Message de...</button>
-        <textarea className='textarea  text-center' placeholder='Votre Message ici'></textarea>
-        <button className='btn m-2 btn-primary '>Envoi Message</button>
+        <textarea className='textarea' placeholder='Votre Message ici'></textarea>
+        <button className='btn m-2 btn-primary'>Envoi Message</button>
       </div>
 
     <div className='div d-flex  justify-content-center flex-column form-control w-50'>
