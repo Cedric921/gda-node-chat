530339c2df3503f212753cb3d07de0787a9cbb27
Cedric karungu <ckarungu921@gmail.com>
2022-10-18 08:33:37 +0200

    refact: fix indetation

--- frontend/src/Envoiemessage.jsx
+++ frontend/src/Envoiemessage.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Envoiemessage extends Component {
-    render() {
-    return (
-      <div>
-<input type="text" className='Mesenv text-primary form-control' placeholder='Votre Message ici'></input>
-<button className='btnenv text-primary'> Envoie Message</button>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<input
+					type='text'
+					className='Mesenv text-primary form-control'
+					placeholder='Votre Message ici'
+				></input>
+				<button className='btnenv text-primary'> Envoie Message</button>
+			</div>
+		);
+	}
 }
