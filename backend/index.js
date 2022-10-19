3438fdcaa488432ea90f4de81536a830d02ff915
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-19 14:12:00 +0200

    feat : add a message on my object

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,41 @@
-// add all modules can be used
 const http = require("http");
-const  url = require("url");
-const  fs = require("fs");
-const uuid = require("uuid")
+const fs = require("fs");
+const uuid = require("uuid");
 
 // importation of messages Object
 
-
-
 const clients = [];
-const PORT = 5000;
+const PORT = 6000;
 
 // Connection to the server
-const server = http.createServer((req,res,next)=>{
-    const {url, method} = req;
-    if(url == "/" && method == 'GET'){
-        let Allmessages = JSON.parse(fs.readFileSync('./messages.json'));
+const server = http.createServer((req, res, next) => {
+  const { url, method } = req;
+  if (url == "/" && method == "GET") {
+    let Allmessages = JSON.parse(fs.readFileSync("./messages.json"));
+    res.write(JSON.stringify(Allmessages));
+    res.end();
+  } else if (url == "/add" && method == "POST") {
+    req.on("data", (client) => {
+      clients.push(client);
+    });
+    req.on("end", () => {
+      const parseBody = JSON.parse(Buffer.concat(clients));
+      let Allmessages = JSON.parse(fs.readFileSync("./messages.json"));
+      const found = Allmessages.find((element) => {
+        return element.uuid == parseBody.uuid;
+      });
+      if (!found) {
+        Allmessages.push(parseBody);
+        fs.writeFileSync("./messages.json", JSON.stringify(Allmessages));
         res.write(JSON.stringify(Allmessages));
         res.end();
-    }else if(url == "/add" && method == 'POST'){
-        req.on("data", (client)=>{
-            clients.push(client);
-        })
-        req.on("end", ()=>{
-            const parseBody = JSON.parse(Buffer.concat(clients))
-            let Allmessages = JSON.parse(fs.readFileSync('./messages.json'))
-            const found = Allmessages.find((element)=>{
-               return element.uuid == parseBody.uuid
-            })
-            if(!found){
-                Allmessages.push(parseBody)
-                fs.writeFileSync("./messages.json", JSON.stringify(Allmessages))
-                res.write(JSON.stringify(Allmessages))
-                res.end();
-            }else {
-                res.write(`This message are already be write`)
-                res.end();
-            }
-        })
-    }
-})
+      } else {
+        res.write(`This message are already be write`);
+        res.end();
+      }
+    });
+  }
+});
 
 server.listen(PORT, () =>
   console.log(`Notre app est lancée sur : http://localhost:${PORT}`)
